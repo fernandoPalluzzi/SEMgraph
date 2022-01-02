1b08526cfaf9bf9a98eef32d36b9fa4ba4581efe
Fernando Palluzzi <51379764+fernandoPalluzzi@users.noreply.github.com>
2022-01-02 18:58:22 +0100

    SEMgraph 1.0.5

--- R/ALSdata.R
+++ R/ALSdata.R
@@ -1,5 +1,5 @@
 #  SEMgraph library
-#  Copyright (C) 2019-2021 Fernando Palluzzi; Mario Grassi
+#  Copyright (C) 2019-2022 Fernando Palluzzi; Mario Grassi
 #  e-mail: <fernando.palluzzi@gmail.com>
 #  University of Pavia, Department of Brain and Behavioral Sciences
 #  Via Bassi 21, 27100 Pavia, Italy
